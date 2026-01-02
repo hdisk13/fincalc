dd7e3788c0d89e592d798d9421e81da03f3c14da
Scott Hunter <jshunter@multicare.org>
2026-01-01 19:19:27 -0800

    trying different things

--- main.go
+++ main.go
@@ -4,20 +4,29 @@ import (
 	"fmt"
 )
 
+/*
+
+ Monthly Payment =  principal * [ interest rate / 12 ]
+				  ---------------------------------
+				  1 - (1 + interest rate / 12)^(-number of months)
+
+
+*/
+
 func main() {
 
 	years := 5
-	interestRate := float64(0.425)
+	interestRate := float64(0.0425)
 	totalAmount := float64(1000.25)
 	months := years * 12
 	payment := float64(39)
 
 	principal := totalAmount
 	for i := 1; i <= months; i++ {
-
+		fmt.Printf("Mnth %d: Starting Balance: %.2f ", i, principal)
 		//remaining balance * (1 + interestRate / 12) - payment
 		principal = principal*(1+interestRate/12) - payment
-		fmt.Printf("Month %d: Remaining Balance: %.2f\n", i, principal)
+		fmt.Printf(": Remaining Balance: %.2f\n", principal)
 	}
 
 }
